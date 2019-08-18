441a6c43320ecbebba50b10c70ac3e16220c7494
albinoty <albinotnoty@gmail.com>
2019-08-18 19:50:36 +0200

    Fix Animations de la section Skill

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,39 +3,24 @@ import image from '../img/carre2.png'
 
 export default class About extends React.Component{
 
-    constructor(){
-        super();
-        this.state =  {
-            titre: false,
-            texte: false,
-            photo: false
-        }
-    }
-
     componentWillMount(){
 
         let cible, effet;
 
         window.addEventListener('scroll', () => {
-            console.log(window.scrollY);
-            console.log(cible)
-
             if(window.scrollY >= 200 && window.scrollY < 500){
-                this.setState({titre: true})
-                cible = document.getElementById('titre');
+                cible = document.getElementById('titreAbout');
                 effet = "fadeInDown";
                 cible.classList.add(effet);
             }
             else if ( window.scrollY >= 501 && window.scrollY < 900){
-                this.setState({texte: true})
-                cible = document.getElementById('texte');
+                cible = document.getElementById('texteAbout');
                 effet = "slideInLeft";
                 cible.classList.add(effet);
-                cible = document.getElementById('photo');
+                cible = document.getElementById('photoAbout');
                 effet = "slideInRight";
                 cible.classList.add(effet);
-            }                
-       
+            }   
         })
     }
 
@@ -43,7 +28,7 @@ export default class About extends React.Component{
         return (
             <section id="about" className="about clair py-5">
                 <div className="container py-4">
-                    <div className="row mx-auto w-75 text-center animated slower" id="titre">
+                    <div className="row mx-auto w-75 text-center animated slower" id="titreAbout">
                         <div className="col-12">
                             <h2 className="text-white text-center d-flex flex-column">
                                 <span className="mx-auto mb-2 line-titre">about</span>
@@ -55,7 +40,7 @@ export default class About extends React.Component{
                         </div>
                     </div>
                     <div className="row mt-5 align-items-center">
-                        <div className="col-lg-7 px-5 animated" id="texte">
+                        <div className="col-lg-7 px-5 animated" id="texteAbout">
                             <p className="">
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, sint necessitatibus fuga nisi fugiat dolor non ab ex molestias at blanditiis ut, sit eveniet, labore nobis voluptate sequi amet harum explicabo. Veritatis repudiandae cumque natus hic sed dicta dignissimos totam laudantium doloribus, temporibus, eaque earum ipsum, molestiae incidunt quas dolore!
                             </p>
@@ -75,7 +60,7 @@ export default class About extends React.Component{
                                 </div>
                             </div>
                         </div>
-                        <div className="col-lg-5 animated" id="photo">
+                        <div className="col-lg-5 animated" id="photoAbout">
                             <img src={image} className="d-block w-100 rounded-circle" alt=""/>
                         </div>
                     </div>
--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -2,11 +2,44 @@ import React from 'react';
 
 export default class Skill extends React.Component{
 
+    constructor(){
+        super();
+        this.state = {
+            bar: false
+        }
+    }
+
+    componentWillMount(){
+
+        let cible, effet;
+
+        window.addEventListener('scroll', () => {
+            console.log(window.scrollY);
+            console.log(cible)
+
+            if(window.scrollY >= 925 && window.scrollY < 1025){
+                cible = document.getElementById('titreSkill');
+                effet = "fadeInDown";
+                cible.classList.add(effet);
+            }
+            else if(window.scrollY >= 1100 && window.scrollY < 1500){
+                cible = document.getElementById('texteSkill');
+                effet = "fadeInDown";
+                cible.classList.add(effet);
+                cible = document.getElementById('barSkill');
+                effet = "fadeInUp";
+                cible.classList.add(effet);
+            }             
+            (window.scrollY >= 1100 && window.scrollY < 2100) ? this.setState({bar: true}) : this.setState({bar: false});
+
+        })
+    }
+
     render(){
         return(
             <section id="skill" className="skill fonce py-5">
                 <div className="container py-4">
-                    <div className={this.props.skill ? 'row mx-auto mb-5 w-75 text-center animated fadeInUp' : ''}>
+                    <div className="row mx-auto mb-5 w-75 text-center animated " id="titreSkill">
                             <div className="col-lg-12">
                                 <h2 className="text-white text-center d-flex flex-column">
                                     <span className="mx-auto mb-2 line-titre">skill</span>
@@ -18,19 +51,19 @@ export default class Skill extends React.Component{
                             </div>
                     </div>
                     <div className="row mt-5 pt-5 align-items-center">
-                        <div className="col-lg-6">
+                        <div className="col-lg-6 animated " id="texteSkill">
                             <h2 className="text-capitalize">Some talk About my professional web development skill</h2>
                             <p className="mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut sed amet alias expedita. Vitae architecto laborum quos, velit nam ratione.</p>
                             <p className="mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut sed amet alias expedita. Vitae architecto laborum quos, velit nam ratione.</p>
                         </div>
-                        <div className="col-lg-6">
+                        <div className="col-lg-6 animated " id="barSkill">
                             {/* HTMl */}
                             <div className="text-white d-flex mt-2">
                                 <span className="d-block mr-auto span">HTML, CSS</span>
                                 <span className="d-block ml-auto span">95%</span>
                             </div>
                             <div className="progress mt-2">
-                                <div className={this.props.bar ? 'progress-bar jaune animated slideInLeft slow': 'progress-bar jaune'} role="progressbar" style={{width: 95 +"%"}} aria-valuenow="95" aria-valuemin="0" aria-valuemax="100">
+                                <div className={this.state.bar ? 'progress-bar jaune animated slideInLeft slow': 'progress-bar jaune'} role="progressbar" style={{width: 95 +"%"}} aria-valuenow="95" aria-valuemin="0" aria-valuemax="100">
                                 </div>
                             </div>
                             {/* js,react */}
@@ -39,7 +72,7 @@ export default class Skill extends React.Component{
                                 <span className="d-block ml-auto span">80%</span>
                             </div>
                             <div className="progress mt-2">
-                                <div className={this.props.bar ? 'progress-bar jaune animated slideInLeft slow' : 'progress-bar jaune'}role="progressbar" style={{width: 80 +"%"}} aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">
+                                <div className={this.state.bar ? 'progress-bar jaune animated slideInLeft slow' : 'progress-bar jaune'}role="progressbar" style={{width: 80 +"%"}} aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">
                                 </div>
                             </div>
                             {/* Java */}
@@ -48,7 +81,7 @@ export default class Skill extends React.Component{
                                 <span className="d-block ml-auto span">55%</span>
                             </div>
                             <div className="progress mt-2">
-                                <div className={this.props.bar ? 'progress-bar jaune animated slideInLeft slow':'progress-bar jaune'} role="progressbar" style={{width: 55 +"%"}} aria-valuenow="55" aria-valuemin="0" aria-valuemax="100">
+                                <div className={this.state.bar ? 'progress-bar jaune animated slideInLeft slow':'progress-bar jaune'} role="progressbar" style={{width: 55 +"%"}} aria-valuenow="55" aria-valuemin="0" aria-valuemax="100">
                                 </div>
                             </div>
                             {/* php,sql */}
@@ -57,7 +90,7 @@ export default class Skill extends React.Component{
                                 <span className="d-block ml-auto span">75%</span>
                             </div>
                             <div className="progress mt-2">
-                                <div className={this.props.bar ? 'progress-bar jaune animated slideInLeft slow' : 'progress-bar jaune'} role="progressbar" style={{width: 75 +"%"}} aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+                                <div className={this.state.bar ? 'progress-bar jaune animated slideInLeft slow' : 'progress-bar jaune'} role="progressbar" style={{width: 75 +"%"}} aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
                                 </div>
                             </div>
                             {/* photshop, illustrator */}
@@ -66,7 +99,7 @@ export default class Skill extends React.Component{
                                 <span className="d-block ml-auto span">70%</span>
                             </div>
                             <div className="progress mt-2">
-                                <div className={this.props.bar ? 'progress-bar jaune animated slideInLeft slow' : 'progress-bar jaune'} role="progressbar" style={{width: 75 +"%"}} aria-valuenow="70" aria-valuemin="0" aria-valuemax="100">
+                                <div className={this.state.bar ? 'progress-bar jaune animated slideInLeft slow' : 'progress-bar jaune'} role="progressbar" style={{width: 75 +"%"}} aria-valuenow="70" aria-valuemin="0" aria-valuemax="100">
                                 </div>
                             </div>
                             
